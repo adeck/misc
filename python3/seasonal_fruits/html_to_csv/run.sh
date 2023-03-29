8a59d067cc44ca8c26f2cde47f755c59c196bcf3
garin <adeck@users.noreply.github.com>
2023-03-28 21:07:55 -0700

    added check for data.csv

--- python3/seasonal_fruits/html_to_csv/run.sh
+++ python3/seasonal_fruits/html_to_csv/run.sh
@@ -2,10 +2,16 @@
 
 set -ex
 
-mkdir venv
-python3 -m venv venv
-. venv/bin/activate
-pip install -r requirements.txt
-./convert.py > ../data.csv
-deactivate
+# Added this check to prevent data from being overwritten, just in case.
+datafile=../data.csv
+[[ -f "$datafile" ]] && {
+    echo "file '$datafile' already exists. Doing nothing."
+} || {
+    mkdir venv
+    python3 -m venv venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+    ./convert.py > ../data.csv
+    deactivate
+}
 
